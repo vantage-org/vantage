ed6a2d396cbcd081a396ffa75fb4ea35b52df3c4
William Mayor <mail@williammayor.co.uk>
2016-05-22 16:25:40 +0100

    Save env to file

--- tests/test_env.sh
+++ tests/test_env.sh
@@ -31,4 +31,26 @@ VG_DEFAULT_ENV=/vagrant/.env/one assert "vantage 'echo \$FOO'" "one"
 temp_env_file=$(vantage __env VG_ENV_FILE)
 assert_raises "[ -f $temp_env_file ]" "1"
 
+# Set env in default env
+touch /vagrant/.env/default
+VG_DEFAULT_ENV=/vagrant/.env/default vantage __env FOO default
+assert "cat /vagrant/.env/default" "FOO=default"
+rm /vagrant/.env/default
+
+# Override set env
+touch /vagrant/.env/default
+VG_DEFAULT_ENV=/vagrant/.env/default vantage __env FOO wrong
+VG_DEFAULT_ENV=/vagrant/.env/default vantage __env FOO default
+assert "cat /vagrant/.env/default" "FOO=default"
+rm /vagrant/.env/default
+
+# Set env in named env
+touch /vagrant/.env/default
+touch /vagrant/.env/named
+VG_DEFAULT_ENV=/vagrant/.env/default vantage -e /vagrant/.env/named __env FOO named
+assert "cat /vagrant/.env/default" ""
+assert "cat /vagrant/.env/named" "FOO=named"
+rm /vagrant/.env/default
+rm /vagrant/.env/named
+
 assert_end env
