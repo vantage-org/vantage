c64ed20ee68bbdbf241c5b300ae7d4f9900307f7
William Mayor <mail@williammayor.co.uk>
2019-10-08 21:39:04 +0100

    feat: add placeholder builtin commands

--- vantage/plugins.py
+++ vantage/plugins.py
@@ -4,3 +4,10 @@ __plugins install NAME_OR_URL
 __plugins update NAME
 __plugins uninstall NAME
 """
+import click
+
+
+@click.command(name="__plugins")
+@click.pass_obj
+def plugins(env, args):
+    raise click.ClickException("Not implemented")
--- vantage/update.py
+++ vantage/update.py
@@ -1,3 +1,10 @@
 """
 __update
 """
+import click
+
+
+@click.command(name="__update")
+@click.pass_obj
+def update(env, args):
+    raise click.ClickException("Not implemented")
