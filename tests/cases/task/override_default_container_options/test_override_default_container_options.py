3e1ef200d862b566301c2b28a0620910326861a5
William Mayor <mail@mayortech.co.uk>
2019-11-18 23:47:01 +0000

    Remove bad test

--- tests/cases/task/override_default_container_options/test_override_default_container_options.py
+++
@@ -1,4 +0,0 @@
-def test_override_default_container_options(stdout):
-    lines = stdout.splitlines()
-    is_found = ["test_override_default_container_options.py" in l for l in lines]
-    assert any(is_found)
