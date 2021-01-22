cd80a2e5d74f89fc34f22c76979c2abf187a9c6e
William Mayor <mail@mayortech.co.uk>
2021-01-22 16:49:24 +0000

    Call the dev install vg-next

--- tasks/install.sh
+++ tasks/install.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 # ---
-# help-text: Install this dev version to /usr/local/bin/v3
+# help-text: Install this dev version to /usr/local/bin/vg-next
 # requires:
 #   - build
 # ---
-sudo rm /usr/local/bin/v3
-sudo ln -s "$VG_APP_DIR/dist/vantage/vantage" /usr/local/bin/v3
+sudo rm /usr/local/bin/vg-next
+sudo ln -s "$VG_APP_DIR/dist/vantage/vantage" /usr/local/bin/vg-next
