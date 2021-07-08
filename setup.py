cdd1920cd2214d465d04e67aa107849f4c96bc32
William Mayor <mail@mayortech.co.uk>
2021-07-08 15:47:35 +0100

    Bump to version 3.2.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="vantage",
-    version="3.0.3",
+    version="3.2.0",
     packages=find_packages(),
     include_package_data=True,
     install_requires=["sh==1.14.2", "ruamel.yaml==0.17.10", "certifi==2021.5.30"],
