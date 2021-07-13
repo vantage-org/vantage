cf1d806fff7faf2d6cdfbf3a450bc673b4ed55eb
William Mayor <mail@mayortech.co.uk>
2021-07-13 11:40:10 +0100

    Fix install script when moving lots of files

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ echo "Removing any existing vantage installations..."
 rm -rf /usr/local/vantage /usr/local/bin/vantage /usr/local/bin/vg
 
 echo "Moving vantage to /usr/local/vantage..."
+mkdir -p /usr/local/vantage
 mv ./vantage ./README.md ./LICENSE /usr/local/vantage
 
 echo "Linking executables to /usr/local/bin/..."
