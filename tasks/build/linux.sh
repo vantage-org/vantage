24686e95ff01a7875a66e34aed265d7b9cb13387
William Mayor <mail@mayortech.co.uk>
2021-07-13 11:52:14 +0100

    Builds are now cross compatible with linux flavours

---
+++ tasks/build/linux.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+# ---
+# image:
+#   tag: "python:3.9"
+#   rm: true
+#   tty: true
+#   interactive: true
+#   volume:
+#     - $VG_APP_DIR:/usr/src/app
+#   workdir: /usr/src/app
+# help-text: Build the vantage binary for Linux (inside a container)
+# ---
+set -eu
+
+echo "Building $VERSION for linux"
+
+rm -rf venv-linux build/x86_64-unknown-linux-gnu
+
+python -m venv venv-linux
+. venv-linux/bin/activate
+pip install -U pip
+pip install pyoxidizer==0.16.2
+
+pyoxidizer build
+
+cp -r build/x86_64-unknown-linux-gnu/debug/install "build/vantage-$VERSION-linux"
+cp install.sh README.md LICENSE "build/vantage-$VERSION-linux/"
+
+cd build
+tar -cvzf "vantage-$VERSION-linux.tar.gz" "vantage-$VERSION-linux"
+
+rm -rf venv-linux
